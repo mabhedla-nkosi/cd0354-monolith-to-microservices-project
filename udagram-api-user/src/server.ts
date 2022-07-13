88940c1e0b3fedc6429aec8e46900eef3633024b
mabhedla-nkosi <nkosi.mlambo@gmail.com>
2022-07-13 13:12:36 +0200

    updates

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -1,20 +1,24 @@
 import cors from 'cors';
 import express from 'express';
-import {sequelize} from './sequelize';
+import sequelize from './sequelize';
 
 import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
 import {config} from './config/config';
-import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+import { V0_USER_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
-  await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
+  try{
+    sequelize.addModels(V0_USER_MODELS);
 
   console.debug("Initialize database connection...");
   await sequelize.sync();
+  }catch(error){
+    console.log(error);
+  }
+  
 
   const app = express();
   const port = process.env.PORT || 8080;
